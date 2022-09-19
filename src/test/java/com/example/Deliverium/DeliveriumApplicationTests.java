860250697bd80d5aff4217aa0dabb40b4f6f6a15
CheReal <72255155+MrVipont@users.noreply.github.com>
2022-09-19 23:55:20 +0300

    Initial commit

---
+++ src/test/java/com/example/Deliverium/DeliveriumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Deliverium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveriumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
